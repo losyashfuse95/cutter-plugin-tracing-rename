32e743c1972512157f03339ade7e2e86a3dbc060
losyashfuse95 <losyashfuse95@gmail.com>
2023-10-30 21:39:12 +0300

    displaying all functions

--- plugin.py
+++ plugin.py
@@ -30,22 +30,34 @@ class MyDockWidget(cutter.CutterDockWidget):
     def populate_table_with_function_data(self, function_data):
         self.table_widget.setRowCount(len(function_data))  # Установка количества строк в таблице
         for row, data in enumerate(function_data):
-            offset = str(data.get("offset", "-"))
-            name = data.get("name", "-")
-            size = str(data.get("size", "-"))
+            address = data.get("address", "-")
+            original_name = data.get("original_name", "-")
+            new_name = data.get("new_name", "-")
+
+            table_item_address = QTableWidgetItem(address)
+            table_item_original_name = QTableWidgetItem(original_name)
+            table_item_new_name = QTableWidgetItem(new_name)
             
-            table_item_offset = QTableWidgetItem(offset)
-            table_item_name = QTableWidgetItem(name)
-            table_item_size = QTableWidgetItem(size)
-            
-            self.table_widget.setItem(row, 0, table_item_offset) 
-            self.table_widget.setItem(row, 1, table_item_name) 
-            self.table_widget.setItem(row, 2, table_item_size) 
+            # Установка элемента в ячейку таблицы
+            self.table_widget.setItem(row, 0, table_item_address) 
+            self.table_widget.setItem(row, 1, table_item_original_name) 
+            self.table_widget.setItem(row, 2, table_item_new_name) 
 
     def update_function_data(self):
-        function_data = cutter.cmdj("aflj")  
-        self.populate_table_with_function_data(function_data)
-
+        function_data = cutter.cmdj("aflj")
+        print(function_data)
+        renamed_functions = MyCutterPlugin.renamed_functions
+        renamed_function_data = [
+            {
+                "address": address,
+                "original_name": func.get("name"),
+                "new_name": renamed_functions.get(address, func.get("name"))
+            }
+            for func in function_data
+            for address in [func.get("address")]
+        ]
+        self.populate_table_with_function_data(renamed_function_data)
+        
     def set_table_width(self):
         header = self.table_widget.horizontalHeader()  # Получение горизонтального заголовка таблицы
         header.setSectionResizeMode(0, QHeaderView.Stretch)  
@@ -61,12 +73,16 @@ class MyDockWidget(cutter.CutterDockWidget):
     def update_new_name(self, row, new_name):
         item = QTableWidgetItem(new_name)
         self.table_widget.setItem(row, 2, item)  # Обновление содержимого ячейки на новое имя
+        address = self.table_widget.item(row, 0).text()
+        MyCutterPlugin.renamed_functions[address] = new_name
 
 class MyCutterPlugin(cutter.CutterPlugin):
     name = "My Plugin"  
     description = "This plugin does awesome things!" 
     version = "1.0" 
     author = "1337 h4x0r" 
+
+    renamed_functions = {}
 
     def setupPlugin(self):
         pass  # Метод для инициализации плагина
